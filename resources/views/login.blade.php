e6b02444bbb7cfd0b8ccd6ec49aa7d4bb0640031
ZahidMahfudz <zaemahfudz@gmail.com>
2023-11-01 08:06:59 +0700

    revisi login dikit

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,13 +6,31 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .mb-1 {
+            margin-bottom: 1rem;
+        }
+
+        .h3 {
+            font-size: 1.5rem;
+        }
+    </style>
     <title>Login</title>
 </head>
 <body>
     <div class="container py-5">
-        <div class="w-50 center border rounded px-3 py-3 mx-auto">
-            <img class="mb-4" src="img/Logo Undip Universitas Diponegoro.png" alt="" width="100" height="100">
-            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+        <div class="w-50 center border rounded px-3 py-3 mx-auto ">
+            <img class="mb-1" src="img/Logo Undip Universitas Diponegoro.png" alt="" width="100" height="100">
+            <h1 class="h3 mb-1 fw-normal text-center">Silahkan Masuk</h1>
         @if ($errors->any())
             <div class="alert alert-danger">
                 <ul>
