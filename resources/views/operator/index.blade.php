ee16d4db6888cfcf8404d4568ea9eb676405bc40
ZahidMahfudz <zaemahfudz@gmail.com>
2023-11-14 22:26:59 +0700

    hehe

--- resources/views/operator/index.blade.php
+++ resources/views/operator/index.blade.php
@@ -1,12 +1,10 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>Dashboard</title>
-  </head>
-  <body>
+@extends('layoutsoperator.main')
+
+@section('main_content')
     <h1>Dashboard.operator</h1>
     <a class="" href="/addmahasiswa">tambah mahasiswa</a>
     <a class="" href="/logout">Sign out</a>
-  </body>
-  </body>
-</html>
+
+@endsection
+
+
